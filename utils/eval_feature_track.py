3cca6891b93db1b380d471e9f6c60ec71f595ea8
Chanwoo Lee <leechanwoo25@gmail.com>
2023-07-18 22:07:52 +0900

    [Fix] eval_feature_track

--- utils/eval_feature_track.py
+++ utils/eval_feature_track.py
@@ -13,15 +13,11 @@ import os
 means = []
 stds = []
 
-def eval_n_plot(out_fn: Path, gt_fn:Path, png_fn: Path):
-
-    out_dir = out_fn.parent
+def eval_n_plot(out_fn: Path, gt_fn:Path, png_fn: Path, save_plot:bool=True):
     out = np.genfromtxt(out_fn, delimiter=',')
-    print("out.shape: ", out.shape)
-
+    # print("out.shape: ", out.shape)
     gt = np.genfromtxt(gt_fn, delimiter=',')
-    print("gt.shape: ", gt.shape)
-
+    # print("gt.shape: ", gt.shape)
     out = torch.from_numpy(out)
     gt = torch.from_numpy(gt)
     gt[:,0] /= 1e9
@@ -39,18 +35,18 @@ def eval_n_plot(out_fn: Path, gt_fn:Path, png_fn: Path):
     est_in = torch.searchsorted(out_ts, tn, right=True) - 1
     assert (out_ts[est_in].item() <= gt_ts[gt_in].item())
 
-    print('t0 ~ tn : %.4f ~ %.4f' % (t0, tn))
+    # print('t0 ~ tn : %.4f ~ %.4f' % (t0, tn))
 
     out_ts = out[est_i0:est_in, 0]
     out_fs = out[est_i0:est_in, 1:3]
     gt_ts = gt[gt_i0:gt_in, 0]
     gt_ps = gt[gt_i0:gt_in, 1:4]
     gt_qs = gt[gt_i0:gt_in, 4:8]
-    print("out_ts.shape: ", out_ts.shape)
-    print("out_fs.shape: ", out_fs.shape)
-    print("gt_ts.shape: ", gt_ts.shape)
-    print("gt_ps.shape: ", gt_ps.shape)
-    print("gt_qs.shape: ", gt_qs.shape)
+    # print("out_ts.shape: ", out_ts.shape)
+    # print("out_fs.shape: ", out_fs.shape)
+    # print("gt_ts.shape: ", gt_ts.shape)
+    # print("gt_ps.shape: ", gt_ps.shape)
+    # print("gt_qs.shape: ", gt_qs.shape)
 
 
     t0 = torch.max(out_ts[0], gt_ts[0]).item()
@@ -61,7 +57,7 @@ def eval_n_plot(out_fn: Path, gt_fn:Path, png_fn: Path):
     n_remove = 0
     while idxs_right[-1 - n_remove] >= len(gt_ts):
         n_remove += 1
-    print("n_remove: ", n_remove)
+    # print("n_remove: ", n_remove)
     if n_remove > 0:
         idxs_left = idxs_left[:-n_remove]
         idxs_right = idxs_right[:-n_remove]
@@ -180,46 +176,48 @@ def eval_n_plot(out_fn: Path, gt_fn:Path, png_fn: Path):
             # print("t:%.4f,  error_3d: %.4f" % (out_ts[i], error_3d))
             # errors.append(error_3d)
 
-        ## 2D Plot
         errors = np.array(errors, dtype=float)
         errors_xs = np.array(errors_xs, dtype=float)
         errors_ys = np.array(errors_ys, dtype=float)
-        # print("mean: %.4f" % errors.mean())
-        fig, axs = plt.subplots(nrows=3, ncols=1, figsize=(9, 9), dpi=200)
-        fig.suptitle('Feature Track Error [px]', fontsize=10)
-        axs[0].set_title('x-axis [px]', {'fontsize':8,'fontweight':2})
-        axs[0].set_ylabel("estimated 'x' [px]", color='b')
-        handle_0_0, = axs[0].plot(gt_ts ,gt_xs, 'k', linewidth=0.6, label="GT")
-        handle_0_1, = axs[0].plot(gt_ts, out_fs[:,0],  'b', linewidth=0.6, label="estimate")
-        axs0_cl = axs[0].twinx()
-        handle_0_2, = axs0_cl.plot(gt_ts, errors_xs,  'r', linewidth=0.6, label="error")
-        axs0_cl.axhline(y=0.0, xmin=0, xmax=1, color='r', linewidth=0.6)
-        axs0_cl.set_ylabel('2d error [px]', color='r')
-        axs0_cl.tick_params('y', colors='r')
-        axs[0].legend(handles=[handle_0_0, handle_0_1, handle_0_2], loc='best', fontsize='small')
 
-        axs[1].set_title('y-axis [px]', {'fontsize':8,'fontweight':2})
-        axs[1].set_ylabel("estimated 'y' [px]", color='b')
-        handle_1_0, = axs[1].plot(gt_ts ,gt_ys, 'k', linewidth=0.6, label="GT")
-        handle_1_1, = axs[1].plot(gt_ts, out_fs[:,1],  'b', linewidth=0.6, label="estimate")
-        axs1_cl = axs[1].twinx()
-        handle_1_2, = axs1_cl.plot(gt_ts, errors_ys,  'r', linewidth=0.6, label="error")
-        axs1_cl.axhline(y=0.0, xmin=0, xmax=1, color='r', linewidth=0.6)
-        axs1_cl.set_ylabel('2d error [px]', color='r')
-        axs1_cl.tick_params('y', colors='r')
-        axs[1].legend(handles=[handle_1_0, handle_1_1, handle_1_2], loc='lower left', fontsize='small')
+        ## 2D Plot
+        if save_plot:
+            # print("mean: %.4f" % errors.mean())
+            fig, axs = plt.subplots(nrows=3, ncols=1, figsize=(9, 9), dpi=200)
+            fig.suptitle('Feature Track Error [px]', fontsize=10)
+            axs[0].set_title('x-axis [px]', {'fontsize':8,'fontweight':2})
+            axs[0].set_ylabel("estimated 'x' [px]", color='b')
+            handle_0_0, = axs[0].plot(gt_ts ,gt_xs, 'k', linewidth=0.6, label="GT")
+            handle_0_1, = axs[0].plot(gt_ts, out_fs[:,0],  'b', linewidth=0.6, label="estimate")
+            axs0_cl = axs[0].twinx()
+            handle_0_2, = axs0_cl.plot(gt_ts, errors_xs,  'r', linewidth=0.6, label="error")
+            axs0_cl.axhline(y=0.0, xmin=0, xmax=1, color='r', linewidth=0.6)
+            axs0_cl.set_ylabel('2d error [px]', color='r')
+            axs0_cl.tick_params('y', colors='r')
+            axs[0].legend(handles=[handle_0_0, handle_0_1, handle_0_2], loc='best', fontsize='small')
 
-        axs[2].set_title('Error [px]', {'fontsize':8,'fontweight':2})
-        axs[2].plot(gt_ts, errors,  'k', linewidth=1, label="GT")
-        axs[2].legend(loc='best')
+            axs[1].set_title('y-axis [px]', {'fontsize':8,'fontweight':2})
+            axs[1].set_ylabel("estimated 'y' [px]", color='b')
+            handle_1_0, = axs[1].plot(gt_ts ,gt_ys, 'k', linewidth=0.6, label="GT")
+            handle_1_1, = axs[1].plot(gt_ts, out_fs[:,1],  'b', linewidth=0.6, label="estimate")
+            axs1_cl = axs[1].twinx()
+            handle_1_2, = axs1_cl.plot(gt_ts, errors_ys,  'r', linewidth=0.6, label="error")
+            axs1_cl.axhline(y=0.0, xmin=0, xmax=1, color='r', linewidth=0.6)
+            axs1_cl.set_ylabel('2d error [px]', color='r')
+            axs1_cl.tick_params('y', colors='r')
+            axs[1].legend(handles=[handle_1_0, handle_1_1, handle_1_2], loc='lower left', fontsize='small')
 
-        # fn = out_dir / 'reprojection_error_%s.png' % out_index
-        # plt.show()
-        # plt.close(fig)
-        fig.tight_layout()
-        fig.savefig(png_fn)
+            axs[2].set_title('Error [px]', {'fontsize':8,'fontweight':2})
+            axs[2].plot(gt_ts, errors,  'k', linewidth=1, label="GT")
+            axs[2].legend(loc='best')
 
-        return errors.mean(), errors.std()
+            # fn = out_dir / 'reprojection_error_%s.png' % out_index
+            # plt.show()
+            # plt.close(fig)
+            fig.tight_layout()
+            fig.savefig(png_fn)
+
+        return errors.mean(), errors.std(), errors.max()
 
         ## 3D Plot
         # out_XS = np.array(out_XS, dtype=float)
@@ -253,12 +251,46 @@ if __name__ == '__main__':
     # Case A #
     ##########
     # gt_fn = Path('/data/test3.csv')
-    # for method in ['correlation', 'haste_correlation', 'haste_correlation_star', 'haste_difference', 'haste_difference_star']:
-    #     out_fn = Path('/data/results/event_feature_tracking/case_a/%s/eval.txt' % method)
-    #     png_dir = Path('./%s/%s' % (dataset, method))
-    #     os.makedirs(png_dir, exist_ok=True)
-    #     png_fn = png_dir / 'errors.png'
-    #     eval_n_plot(out_fn, gt_fn, method)
+    # for method in ['EFTv0']:
+    #     for dataset, ids in {'case_a': ['230718_110256']}.items():
+    #         for id in ids:
+    #             out_fn = Path('/data/results/event_feature_tracking/case_a/%s/%s/0.csv' % (method, id))
+    #             png_dir = Path('./results/%s/%s/%s' % (dataset, method, id))
+    #             os.makedirs(png_dir, exist_ok=True)
+    #             png_fn = png_dir / 'errors.png'
+    #             mean_, std_ = eval_n_plot(out_fn, gt_fn, png_fn)
+    #             print("[%s](%s-%s) means:%.4f, std:%.4f" % (method, dataset, id, mean_, std_))
+
+    # case_A_dict = {
+    #     'checkerboard_rhombus_dt01': ['230717_103644'],
+    #     'checkerboard_rhombus_dt02': ['230717_103610'],
+    #     'checkerboard_rhombus_dt03': ['230717_103731'],
+    #     'checkerboard_rhombus_dt04': ['230717_103830'],
+    #     'checkerboard_rhombus_dt05': ['230717_104034'],
+    #     'checkerboard_rhombus_dt06': ['230717_104118'],
+    #     'checkerboard_rhombus_dt07': ['230717_104224'],
+    #     'checkerboard_rhombus_dt08': ['230717_104835'],
+    #     'checkerboard_rhombus_dt09': ['230717_104920'],
+    #     'checkerboard_rhombus_dt10': ['230717_105157'],
+    # }
+    # for method in ['EFTv0']:
+    #     for dataset, ids in case_A_dict.items():
+    #         gt_fn = '/data/datasets/dataset_ours/dt_%.1f.csv' % (float(dataset[-2:]) / 10.0)
+    #         for id in ids:
+    #             out_fn = Path('/data/results/event_feature_tracking/%s/%s/%s/0.csv' % (dataset, method, id))
+    #             out_vfn = Path('/data/results/event_feature_tracking/%s/%s/%s/0v.csv' % (dataset, method, id))
+    #             png_dir = Path('./%s/%s/%s' % (dataset, method, id))
+    #             # print("out_fn: ", out_fn)
+    #             # print("out_vfn: ", out_vfn)
+    #             # print("png_dir: ", png_dir)
+    #             os.makedirs(png_dir, exist_ok=True)
+    #             png_fn = png_dir / 'errors.png'
+    #             mean_, std_ = eval_n_plot(out_fn, gt_fn, png_fn)
+    #             means.append(mean_)
+    #             stds.append(std_)
+    # print("means:\n", means)
+    # print("stds:\n", stds)
+
 
     ##########
     # Case B #
@@ -267,7 +299,7 @@ if __name__ == '__main__':
     #     for dataset in ['case_B_dt05']:
     #         gt_fn = Path('/data/datasets/dataset_ours/case_B_dt_05.csv')
     #         out_fn = Path('/data/results/event_feature_tracking/%s/%s/eval.txt' % (dataset, method))
-    #         png_dir = Path('./%s/%s' % (dataset, method))
+    #         png_dir = Path('./results/%s/%s' % (dataset, method))
     #         os.makedirs(png_dir, exist_ok=True)
     #         png_fn = png_dir / 'errors.png'
     #         eval_n_plot(out_fn, gt_fn, png_fn)
@@ -275,7 +307,7 @@ if __name__ == '__main__':
     #     for dataset in ['case_B_dt10']:
     #         gt_fn = Path('/data/datasets/dataset_ours/case_B_dt_10.csv')
     #         out_fn = Path('/data/results/event_feature_tracking/%s/%s/eval.txt' % (dataset, method))
-    #         png_dir = Path('./%s/%s' % (dataset, method))
+    #         png_dir = Path('./results/%s/%s' % (dataset, method))
     #         os.makedirs(png_dir, exist_ok=True)
     #         png_fn = png_dir / 'errors.png'
     #         eval_n_plot(out_fn, gt_fn, png_fn)
@@ -284,7 +316,7 @@ if __name__ == '__main__':
     #     dataset = 'case_B_dt05'
     #     gt_fn = Path('/data/datasets/dataset_ours/case_B_dt_05.csv')
     #     out_fn = Path('/data/results/event_feature_tracking/case_B_dt05/EFTv0/230716_230727/0.csv')
-    #     png_dir = Path('./%s/%s' % (dataset, method))
+    #     png_dir = Path('./results/%s/%s' % (dataset, method))
     #     os.makedirs(png_dir, exist_ok=True)
     #     png_fn = png_dir / 'errors.png'
     #     eval_n_plot(out_fn, gt_fn, png_fn)
@@ -292,7 +324,7 @@ if __name__ == '__main__':
     #     dataset = 'case_B_dt10'
     #     gt_fn = Path('/data/datasets/dataset_ours/case_B_dt_10.csv')
     #     out_fn = Path('/data/results/event_feature_tracking/case_B_dt10/EFTv0/230716_230810/0.csv')
-    #     png_dir = Path('./%s/%s' % (dataset, method))
+    #     png_dir = Path('./results/%s/%s' % (dataset, method))
     #     os.makedirs(png_dir, exist_ok=True)
     #     png_fn = png_dir / 'errors.png'
     #     eval_n_plot(out_fn, gt_fn, png_fn)
@@ -319,15 +351,13 @@ if __name__ == '__main__':
             for id in ids:
                 out_fn = Path('/data/results/event_feature_tracking/%s/%s/%s/0.csv' % (dataset, method, id))
                 out_vfn = Path('/data/results/event_feature_tracking/%s/%s/%s/0v.csv' % (dataset, method, id))
-                png_dir = Path('./%s/%s/%s' % (dataset, method, id))
+                png_dir = Path('./results/%s/%s/%s' % (dataset, method, id))
                 # print("out_fn: ", out_fn)
                 # print("out_vfn: ", out_vfn)
                 # print("png_dir: ", png_dir)
                 os.makedirs(png_dir, exist_ok=True)
                 png_fn = png_dir / 'errors.png'
-                mean_, std_ = eval_n_plot(out_fn, gt_fn, png_fn)
+                mean_, std_, max_ = eval_n_plot(out_fn, gt_fn, png_fn, save_plot=False)
+                print("[%s](%s-%s) means:%.4f, std:%.4f, max:%.4f" % (method, dataset, id, mean_, std_, max_))
                 means.append(mean_)
                 stds.append(std_)
-
-    print("means:\n", means)
-    print("stds:\n", stds)
